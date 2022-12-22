94f574152bc058dba19a73c2eb685c4545ba74c0
Ankita-01Mishra <boycaptain789@gmail.com>
2022-12-23 01:24:39 +0530

    to do committed

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 document.querySelector('#popup').style.display="none"
 document.querySelector('#popup2').style.display="none"
+document.querySelector('#headeroneDiv').style.display="none"
+// document.querySelector('#oneMainDiv').style.display="none"
 
-document.getElementById('btn').addEventListener('click',(e)=>{
+document.querySelector('.btn').addEventListener('click',(e)=>{
     e.preventDefault();
     // console.log('clicked');
     document.querySelector('#blur').style.filter="blur(7px)"
@@ -18,7 +20,7 @@ function closePopup1(){
 // let main=document.createElement("main")
 const taskList=[];
 let itemCount=0;
-let count=1;
+
 
 //creating div box
 function addBox(){
@@ -34,14 +36,8 @@ document.querySelector('#popup').style.display="none";
 document.querySelector('#my-para').style.display="none";
 document.querySelector('#blur').style.filter="blur(0px)";
 let notes=document.getElementById("notes");
-notes.innerHTML += `<div class="noteCard card" id=${itemCount} style="width: 15rem;  
-height:17rem; background-color:white; color:black; margin-right:10px; margin-bottom:20px">
-<center>
-    <span style="font-size:18px" class="card-text">${cardTitle}</span>
-</center>    
-    <hr>
-   <ul class="tasks" id="itemList${itemCount}" style="height:150px; border: 2px solid red;font-size:10px "></ul><i class="fa-solid fa-plus" onclick="popup2(); idList(event)" id="add_btn_${itemCount++}" style="cursor: pointer;float:right;margin-top:5px;margin-right:5px;background-color:blue;border-radius:50%;color:white;font-size:25px "></i>
-    <i  onclick="deleteNote(this) " class="fa-solid fa-trash-can dltBtn" style="cursor: pointer;float:right;margin-top:5px;margin-right:5px;border-radius:50%;font-size:25px;color:red; "></i>
+notes.innerHTML += `<div class="noteCard card" id=${itemCount} style="width: 15rem;height:17rem; background-color:white; color:black; margin-right:10px; margin-bottom:20px; "><center><span style="font-size:25px;color: #7e3e06;font-family: cursive;" class="card-text" onclick="particularDiv(this)">${cardTitle}</span></center><hr><ul class="tasks" id="itemList${itemCount}" style="height:150px;font-size:10px "></ul><i class="fa-solid fa-plus" onclick="popup2(); idList(event)" id="add_btn_${itemCount++}" style="cursor: pointer;float:right;margin-top:5px;margin-right:5px;background-color:blue;border-radius:50%;color:white;font-size:25px "></i>
+    <i  onclick="deleteNote(this) " class="fa-solid fa-trash-can dltBtn" style="cursor: pointer;float:right;margin-top:5px;margin-right:10px;border-radius:50%;font-size:25px;color:red; "></i>
 </div>`
 }
 
@@ -76,8 +72,9 @@ function idList(plusiconOfDiv){
     console.log(createList);
 }
 
+let listcount=1;
 //Adding list into the created ul
-let addItem=()=>{
+let addItemLikeList=()=>{
     document.querySelector('#blur').style.filter="blur(0px)"
     document.querySelector('#popup2').style.display="none"
     console.log('clicked');
@@ -85,9 +82,10 @@ let addItem=()=>{
     console.log(addTxt2);
     let li=document.createElement("li");
     document.getElementById(createList).appendChild(li);
-    li.id=`item_${count++}`
+    li.id=`item_${listcount++}`
     // console.log(li.id,li,"li.id");
-    li.innerHTML=`<span>${addTxt2}</span><button id="markDone" onclick="cutIfWorkDone(this)">Markdone</button>`
+    li.style.listStyleType="none"
+    li.innerHTML=`<span style="font-size:15px; padding-left:56px">${addTxt2}</span><button id="markDone" onclick="cutIfWorkDone(this)">Markdone</button>`
 }
 
 //Markdone function
@@ -98,9 +96,46 @@ let cutIfWorkDone=(index)=>{
 
 
 
+let particularDiv=(specificDiv)=>{
+    console.log('pd created');
+    console.log(specificDiv.parentElement.parentElement.innerHTML);
+    let specificDivValue=specificDiv.innerHTML
+    console.log(specificDivValue);
+    console.log(specificDiv.parentElement.nextSibling.nextSibling.innerHTML);
+    console.log(specificDiv.parentElement.parentElement.id);
+    document.querySelector('#blur').style.visibility="hidden"
+    document.querySelector('#headeroneDiv').style.display="block"
+    document.querySelector('#oneMainDiv').style.display="block"
+    // specificDiv.parentElement.parentElement.id.style.visibility="visible"
+    document.querySelector('#addParticularHeaderValue').innerHTML=specificDiv.innerHTML;
+    document.querySelector('#headeroneDiv').style.color="white"
+    document.querySelector('#headeroneDiv').style.display="flex"
+    document.querySelector('#headeroneDiv').style.justifyContent="space-around"
+    document.querySelector('#headeroneDiv').style.fontSize="31px"
 
+    let oneMainDiv=document.getElementById('oneMainDiv')
+    oneMainDiv.innerHTML +=`
+    <div id="oneDiv">
+    ${specificDiv.parentElement.parentElement.innerHTML}
+    </div>
+    `
+}
 
+function Back(){
+    document.querySelector('#blur').style.visibility="visible"
+    document.querySelector('#headeroneDiv').style.display="none"
+    document.querySelector('#oneMainDiv').style.display="none"
+}
 
+document.querySelector('.btnLast').addEventListener('click',(e)=>{
+    e.preventDefault();
+    // console.log('clicked');
+    document.querySelector('#headeroneDiv').style.display="none"
+    document.querySelector('#oneMainDiv').style.display="none"
+    document.querySelector('#blur').style.visibility="visible"
+    document.querySelector('#blur').style.filter="blur(7px)"
+    document.querySelector('#popup').style.display="block"
+})
 
 
 
